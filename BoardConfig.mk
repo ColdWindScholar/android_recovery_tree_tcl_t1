a9628043e19dc20051f3c897cac6ec47bb5f251b
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2025-05-04 13:48:54 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tcl/TCL_T1
+DEVICE_PATH := device/TCL/TCL_T1
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
